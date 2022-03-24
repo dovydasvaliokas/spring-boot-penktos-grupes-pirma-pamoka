89bc5c1ea9333d5e9ff7c7e3a76b223dbc204359
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-03-24 12:42:15 +0200

    sugeneruotas spring boot projektas

---
+++ src/main/java/lt/viltiesziedas/springbootpamoka1pradziairjpa/SpringBootPamoka1PradziaIrJpaApplication.java
@@ -0,0 +1,13 @@
+package lt.viltiesziedas.springbootpamoka1pradziairjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPamoka1PradziaIrJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPamoka1PradziaIrJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/viltiesziedas/springbootpamoka1pradziairjpa/SpringBootPamoka1PradziaIrJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.viltiesziedas.springbootpamoka1pradziairjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPamoka1PradziaIrJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
